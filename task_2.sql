a45b846f693ea025a8b1130b9d26de3303ded98c
angelgracecy <angelgracecy@gmail.com>
2024-07-27 10:47:42 -0700

    database

--- task_2.sql
+++ task_2.sql
@@ -36,13 +36,11 @@ CREATE TABLE IF NOT EXISTS Orders (
 );
 
 -- Create order_details table
-CREATE TABLE IF NOT EXISTS Order_details (
-    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Order_Details (
+	orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT,
-    price DECIMAL(10, 2),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-
